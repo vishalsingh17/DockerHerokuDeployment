f6062b8aad249722059d5e1f4946fdcca290a252
unknown <vishal170997@gmail.com>
2022-06-30 15:28:03 +0530

    updated app.py

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return 'Jai Shri RAM'
+    return render_template('welcome.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    return render_template('login.html')
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    return render_template('signup.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
